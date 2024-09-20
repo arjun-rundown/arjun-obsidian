b6f5e75482cd7ec09b2342f303f9c54fbcbbfdfa
arjun-rundown <arjunsingh@rundown.co.in>
2024-09-21 00:05:38 +0530

    Quartz sync: Sep 21, 2024, 12:05 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "arjun-rundown/github.io/arjun-obsidian",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
